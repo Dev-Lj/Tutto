7bb23fe0f2d51b69626ea9b4cb13a03241673ab0
Atomis14 <michelsabbatini@outlook.com>
2022-11-29 16:38:55 +0100

    implemented default deck

--- src/main/java/ch/uzh/deck/Deck.java
+++ src/main/java/ch/uzh/deck/Deck.java
@@ -1,10 +1,10 @@
 package ch.uzh.deck;
 
-import java.util.Arrays;
-import java.util.List;
+import ch.uzh.turn.turnStrategy.*;
+
+import java.util.*;
 
 public class Deck {
-
     private final List<Card> cards;
 
     private Deck(List<Card> cards) {
@@ -12,16 +12,41 @@ public class Deck {
     }
 
     public static Deck createDefaultDeck() {
-        Card[] defaultCards = {};
-        return new Deck(Arrays.asList(defaultCards));
+        Map<Card, Integer> cardFrequencies = new HashMap<>()
+        {{
+            put(new Card("Bonus 200", new TuttoBonusTurn(200)), 5);
+            put(new Card("Bonus 300", new TuttoBonusTurn(300)), 5);
+            put(new Card("Bonus 400", new TuttoBonusTurn(400)), 5);
+            put(new Card("Bonus 500", new TuttoBonusTurn(500)), 5);
+            put(new Card("Bonus 600", new TuttoBonusTurn(600)), 5);
+            put(new Card("x2", new TuttoDoubleTurn()), 5);
+            put(new Card("Stop", new StopTurn()), 10);
+            put(new Card("Fireworks", new FireworksTurn()), 5);
+            put(new Card("Plus/Minus", new PlusMinusTurn()), 5);
+            put(new Card("Cloverleaf", new CloverleafTurn()), 1);
+            put(new Card("Straight", new StraightTurn()), 5);
+        }};
+
+        List<Card> defaultCards = new ArrayList<>();
+        for(Map.Entry<Card, Integer> card : cardFrequencies.entrySet()) {
+            for(int i = 0; i < card.getValue(); i++) {
+                defaultCards.add(card.getKey());
+            }
+        }
+        Collections.shuffle(defaultCards);
+
+        return new Deck(defaultCards);
     }
 
     public Card drawCard() {
-        return cards.remove(0);
+        Card drawnCard = cards.remove(0);
+        cards.add(drawnCard);
+        return drawnCard;
     }
 
+    // TODO what is this used for?
     public void shuffle() {
-        // TODO implement
+        Collections.shuffle(cards);
     }
 
 
