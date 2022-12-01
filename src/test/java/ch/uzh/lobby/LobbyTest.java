f0b5a024bacb05e8d3f5a7bc5769814e841318dd
Atomis14 <michelsabbatini@outlook.com>
2022-12-01 18:46:27 +0100

    tried to implement tests for Lobby ...

--- src/test/java/ch/uzh/lobby/LobbyTest.java
+++ src/test/java/ch/uzh/lobby/LobbyTest.java
@@ -1,34 +1,50 @@
 package ch.uzh.lobby;
 
+import ch.uzh.deck.Deck;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LobbyTest {
+    /*List<Player> players = Arrays.asList(new Player("")); // TODO is it okay to use real players...?
+    Lobby lobby = MockLobby.create();
+    Lobby createLobby() {
+        Player p1 = new Player("Ahmed");
+        Player p2 = new Player("");
+    }*/
+
+
     void setUserInput(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
     }
 
+    // TODO how to test because userInput always gets tested along...???
     @Test
-    void testCreateNewLobby() {
+    void testCreateNewLobby() throws NoSuchFieldException {
         setUserInput("2\nMichel\nDavid\n");
         Lobby lobby = Lobby.createNewLobby();
+        Field playersField = Lobby.class.getDeclaredField("players");
         //Metaprogramming set nrOfPlayers
     }
 
     @Test
     void testGetNextPlayer() {
+        // use mock lobby
     }
 
     @Test
-    void testGetPlayersSortedByRank() {
+    void testGetPlayersSortedByRank() { // TODO WHAT TO DO HEREEEEEE :(
+        // use mock lobby
     }
 
     @Test
     void testSetWinner() {
+        // use mock lobby?
     }
 }
