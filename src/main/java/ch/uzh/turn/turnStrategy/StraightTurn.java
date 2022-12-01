bdc6a04f19b05f96b32d4efe4e873e9be2df5a71
David Scherrer <davidscherrer@gmx.ch>
2022-12-01 17:15:12 +0100

    started Unit Tests for StraighTurn

--- src/main/java/ch/uzh/turn/turnStrategy/StraightTurn.java
+++ src/main/java/ch/uzh/turn/turnStrategy/StraightTurn.java
@@ -1,8 +1,6 @@
 package ch.uzh.turn.turnStrategy;
 
-import java.util.Arrays;
-import java.util.List;
-
+import ch.uzh.ConsoleInput;
 import ch.uzh.command.Command;
 import ch.uzh.command.NullCommand;
 import ch.uzh.dice.DiceManager;
@@ -11,22 +9,35 @@ import ch.uzh.turn.PlayerTurn;
 import ch.uzh.turn.TurnStrategy;
 
 public class StraightTurn implements TurnStrategy{
-    int score;
+    private int score;
+    private DiceManager aDiceManager;
+
+    public StraightTurn(){
+        this.score = 0;
+        this.aDiceManager = new DiceManager(6, new StraightDiceScoreStrategy());
+    };
 
     @Override
-    public Command playTurn(PlayerTurn currentTurn) {
-        DiceManager aDiceManager = new DiceManager(6, new StraightDiceScoreStrategy());
+    public Command playTurn(PlayerTurn currentTurn) {       
+        aDiceManager = turnLoop(aDiceManager, ConsoleInput.instance());
+        return evaluateTurn(aDiceManager, currentTurn); //evaluateTurn returns Command
+    }
 
+    private DiceManager turnLoop(DiceManager aDiceManager, ConsoleInput aConsoleInput){
         while(!aDiceManager.hadNullTurn() && !aDiceManager.isTutto()){
             // Ask user to roll the Dice
             Character[] acceptedInputs = {'R'};
-            getUserInput(acceptedInputs,"Please enter R to roll the dice. You're not allowed to stop your turn.");
+            aConsoleInput.getCharacterInput(acceptedInputs,"Please enter R to roll the dice. You're not allowed to stop your turn.");
 
             // Roll and Display Dice
             aDiceManager.rollDice();
             aDiceManager.printScoredDices();
+            
         }
+        return aDiceManager;
+    }
 
+    private Command evaluateTurn(DiceManager aDiceManager, PlayerTurn currentTurn){
         if (aDiceManager.isTutto()){
             score = 2000;
         } else {
@@ -41,29 +52,8 @@ public class StraightTurn implements TurnStrategy{
         return score;
     }
 
-    public char getUserInput(Character[] acceptedInputs, String message){
-        List<Character> acceptedInputsList = Arrays.asList(acceptedInputs);
-        Boolean inputAccepted;
-        String input = "";
-        do {
-            try{
-                System.out.println(message);
-                input = System.console().readLine();
-                if (input.length() != 1){
-                    throw new IllegalArgumentException("Please only enter one character.");
-                }
-                if (!acceptedInputsList.contains(input.charAt(0))){
-                    throw new IllegalArgumentException("The character you entered wasn't one of the possible options.");
-                }
-                inputAccepted = true;
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.out.println("Let's try again...\n");
-                inputAccepted = false;
-            }
-        } while(!inputAccepted);
-
-        return (char) input.charAt(0);
-    }   
+    private void setScore(int aScore) {
+        this.score = aScore;
+    }
     
 }
--- src/test/java/ch/uzh/turn/turnStrategy/StraightTurnTest.java
+++ src/test/java/ch/uzh/turn/turnStrategy/StraightTurnTest.java
@@ -1,8 +1,13 @@
 package ch.uzh.turn.turnStrategy;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.Test;
+
+import ch.uzh.turn.TurnStrategy;
 
 /**
  * Unit test for simple App.
@@ -13,8 +18,14 @@ public class StraightTurnTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    void testGetScore()
     {
-        assertTrue( true );
+        StraightTurn aTurn = new StraightTurn();
+        Method scoreField = StraightTurn.class.getDeclaredMethod("getScore");
+        
+        scoreField.setAccessible(true);
+
+
+        assertEquals(100, aTurn.getScore());
     }
 }
