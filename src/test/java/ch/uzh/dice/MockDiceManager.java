523bb7224d339f8f6e28f5960107f6ff144809f4
David Scherrer <davidscherrer@gmx.ch>
2022-12-02 16:02:50 +0100

    Minor changes

--- src/test/java/ch/uzh/dice/MockDiceManager.java
+++ src/test/java/ch/uzh/dice/MockDiceManager.java
@@ -2,16 +2,25 @@ package ch.uzh.dice;
 
 
 public class MockDiceManager extends DiceManager {
-    private boolean isTutto;
+    private boolean MockisTutto;
+    private boolean MockHadNullTurn;
     public MockDiceManager(DiceScoreStrategy aDiceScoreStrategy) {
         super(6, aDiceScoreStrategy);
     }
 
+    public void setHadNullTurn(boolean aMockHadNullTurn) {
+        this.MockHadNullTurn = aMockHadNullTurn;
+    }
+
+    public boolean hadNullTurn() {
+        return MockHadNullTurn;
+    }
+
     public void setIsTutto(boolean aTutto) {
-        this.isTutto = aTutto;
+        this.MockisTutto = aTutto;
     }
 
     public boolean isTutto() {
-        return isTutto;
+        return MockisTutto;
     }
 }
