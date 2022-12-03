fca426d4ff1af154952315e5ce8155c7e5c3464d
lj <janluethi2000@gmail.com>
2022-12-03 20:05:26 +0100

    TestingConsoleInput to private class

--- src/test/java/ch/uzh/TestingConsoleInput.java
+++ src/test/java/ch/uzh/TestingConsoleInput.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TestingConsoleInput {
+    private TestingConsoleInput(){}
 
     /**
      * Creates instance of ConsoleInput with predefined input.
